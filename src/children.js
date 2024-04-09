3e7d6b92d5d73537c1e2aa92af5c9002886fb628
MarcN04 <niedermeier.marc@googlemail.com>
2024-04-09 21:34:47 +0200

    initial commit

---
+++ src/children.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const E5113 = ({ children }) => {
+  return <div className="e5_113">{children}</div>;
+};
+
+const E5114 = ({ children }) => {
+  return <div className="e5_114">{children}</div>;
+};
+
+const E5117 = ({ children }) => {
+  return <div className="e5_117">{children}</div>;
+};
+
+const E5118 = ({ children }) => {
+  return <span className="e5_118">{children}</span>;
+};
+
+const E6143 = ({ children }) => {
+  return <div className="e6_143">{children}</div>;
+};
+
+const E6144 = ({ children }) => {
+  return <span className="e6_144">{children}</span>;
+};
+
+const E6147 = ({ children }) => {
+  return <div className="e6_147">{children}</div>;
+};
+
+const E6148 = ({ children }) => {
+  return <span className="e6_148">{children}</span>;
+};
+
+const E6149 = ({ children }) => {
+  return <span className="e6_149">{children}</span>;
+};
+
+export { E5113, E5114, E5117, E5118, E6143, E6144, E6147, E6148, E6149 };
