3e29767479879e9d95e5e90816a8501f108ff7a2
MarcN04 <marc-niedermeier@web.de>
2024-04-29 22:41:45 +0200

    test amplify integration

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import React, { useState } from 'react';
+import { Amplify } from 'aws-amplify';
 import { put } from 'aws-amplify/api';
 
-// import { E5113, E5114, E5117, E5118, E6143, E6144, E6147, E6148, E6149 } from './children';
 
-// const App = () => {
-//   return (
-//     <html lang="en">
-//       <head>
-//         <meta charset="utf-8" />
-//         <title>Html Generated</title>
-//         <meta name="description" content="Figma htmlGenerator" />
-//         <meta name="author" content="htmlGenerator" />
-//         <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
-//         <link rel="stylesheet" href="./styles.css" />
-//       </head>
-//       <body>
-//         <E5113>
-//           <E5114>
-//             <E5117>
-//               <span className="e5118">Gordon</span>
-//             </E5117>
-//             <E6143>
-//               <span className="e6144">Junge</span>
-//             </E6143>
-//             <E6147>
-//               <span className="e6148">Mädchen</span>
-//             </E6147>
-//           </E5114>
-//           <span className="e6149">Worum handelt es sich denn bei dem Hauptcharakter?</span>
-//         </E5113>
-//       </body>
-//     </html>
-//   );
-// };
-
+Amplify.configure({
+  API: {
+    REST: {
+      childrenbookstoreapi: {
+        endpoint:
+          'https://8okigt8yug.execute-api.eu-north-1.amazonaws.com/staging',
+        region: 'eu-north-1' // Optional
+      }
+    }
+  }
+});
 
 function App() {
   const [formData, setFormData] = useState({
