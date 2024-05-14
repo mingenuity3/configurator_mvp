0679d1e2687d5caa49fc5372c5d04fb49c075253
matthiasgordon <gordon.matthias@googlemail.com>
2024-05-14 22:45:20 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React, { useState } from 'react';
 import { post } from 'aws-amplify/api';
 
 function App() {
-  const [formData, setFormData] = useState({
-    name: '',
-    gender: '',
-    topic: ''
-  });
+  const [question] = useState("Wer ist der coolste hier?");
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [options] = useState([
+    { id: 1, text: 'Marc' },
+    { id: 2, text: 'Matthias' },
+    { id: 3, text: 'Matthes' },
+  ]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
+  const handleOptionSelect = (optionText) => {
+    setSelectedOption(optionText);
   };
 
   const handleSubmit = async (e) => {
@@ -23,18 +21,13 @@ function App() {
         apiName: "bookstoreapi",
         path: "/book/123",
         options: {
-          body: { formData },
+          body: { selectedOption },
         }
       });
       
-      console.log('Request:', formData);
+      console.log('Request:', selectedOption);
       console.log('Response:', response);
       // Optionally, reset the form after successful submission
-      setFormData({
-        name: '',
-        gender: '',
-        topic: ''
-      });
     } catch (error) {
       console.error('Error:', error);
     }
@@ -42,36 +35,25 @@ function App() {
 
   return (
     <><link rel="stylesheet" href="./styles.css" /><div className="App">
-      <h1>Submit Form</h1>
+      <h1>{question}</h1>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="gender">Gender:</label>
-          <input
-            type="text"
-            id="gender"
-            name="gender"
-            value={formData.gender}
-            onChange={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="topic">Topic:</label>
-          <input
-            type="text"
-            id="topic"
-            name="topic"
-            value={formData.topic}
-            onChange={handleChange} />
-        </div>
-        <button type="submit">Submit</button>
+        <ul>
+        {options.map((option) => (
+          <li
+            key={option.id}
+            className={selectedOption === option.text ? 'selected' : ''}
+            onClick={() => handleOptionSelect(option.text)}
+          >
+            {option.text}
+          </li>
+        ))}
+        </ul>
+        {selectedOption && (
+        <p>
+          Ausgesucht: {selectedOption}
+        </p>
+        )}
+        <button type="submit">Ab inne Datenbank</button>
       </form>
     </div></>
   );
