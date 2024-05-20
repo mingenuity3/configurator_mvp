a5e8b4f28cb24e41abb10f9e1dc6c69803ce5f5d
MarcN04 <marc-niedermeier@web.de>
2024-05-20 11:35:08 +0200

    adapt formatting & rename variables to Python style

--- amplify/backend/function/bookHandler/src/index.py
+++ amplify/backend/function/bookHandler/src/index.py
@@ -4,47 +4,50 @@ import json
 dynamodb = boto3.client('dynamodb')
 
 def handler(event, context):
-    CallerMethod = event['httpMethod']
-    ConstantGET = 'GET'
-    ConstantPOST = 'POST'
+    caller_method = event['httpMethod']
+    constant_get = 'GET'
+    constant_post = 'POST'
     
-    db_response = ""
-    
-    if CallerMethod == ConstantGET:
-        BookId = event['pathParameters']['book-id']
-        db_response = dynamodb.get_item(
-            TableName='childrenbooks-dev',
-            Key={
-                'book-id': {
-                    'S': BookId
+    try:
+        if caller_method == constant_get:
+            book_id = event['pathParameters'].get('book-id', '')
+            db_response = dynamodb.get_item(
+                TableName='childrenbooks-dev',
+                Key={
+                    'book-id': {'S': book_id}
                 }
-            }
+            )
+            
+        elif caller_method == constant_post:
+            book_id = event['pathParameters'].get('book-id', '')
+            body = json.loads(event['body'])
+            name = body.get('selectedOption', '')
+            db_response = dynamodb.put_item(
+                TableName='childrenbooks-dev',
+                Item={
+                    'book-id': {'S': book_id},
+                    'name': {'S': name}
+                }
             )
         
-    if CallerMethod == ConstantPOST:
-        BookId = event['pathParameters']['book-id']
-        #Name = event['headers']['name']
-        #Name = event['body'].get('name')
-        ParsedJson = json.loads(event['body'])
-        Name = ParsedJson['selectedOption']
-        db_response = dynamodb.put_item(
-            TableName='childrenbooks-dev',
-            Item={
-                'book-id': {
-                    'S': BookId
-                },
-                'name': {
-                    'S': Name
-                }
-            }
-            )
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps(db_response)
+        }
     
-    return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps(db_response)
-  }
+    except Exception as e:
+        # Handle any exceptions
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps({'error': str(e)})
+        }
--- src/App.js
+++ src/App.js
@@ -59,7 +59,4 @@ function App() {
   );
 }
 
-// export default App;
-
-
 export default App;
