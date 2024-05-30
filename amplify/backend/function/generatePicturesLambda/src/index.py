378ab913c205808398c4daa7e530e18b908ee6f3
matthiasgordon <gordon.matthias@googlemail.com>
2024-05-31 00:45:43 +0200

    New API for image creation

---
+++ amplify/backend/function/generatePicturesLambda/src/index.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+import requests
+
+
+
+def handler(event, context):
+
+    response = requests.post(
+    f"https://api.stability.ai/v2beta/stable-image/generate/sd3",
+    headers={
+        "authorization": f"Bearer sk-d4L1y0TpKJH8VWuhykWzktsuNCHbIj14Wgm1P2kNH0jR86bt",
+        "accept": "image/*"
+    },
+    files={"none": ''},
+    data={
+        "prompt": "character from a childbook",
+        "output_format": "jpeg",
+    },
+    )
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps(response)
+    }
